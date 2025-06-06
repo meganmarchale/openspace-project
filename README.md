eab054e740584bdc15c2e16425344b5bdceeeda4
Megan <megan.marchale@gmail.com>
2025-06-06 16:52:54 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,25 +1,2 @@
 # openspace-project
-
-### Structure ideas
-
-Create a Parent class openspace():
-    1. Define a method wich would call the file
-    2. Define the needs
-
-Child class table
-    1. Tables are created depending on the needs 
-    2. Limit the number of tables to 7
-
-Child class seats
-    1. Function 1 : randomly attributes the people to the tables
-    
-                condition : 1 person cannot sit alone
-                if there are more than 28 people in the list:
-                    we have to redirect the remaining people to another room
-                        print list of the names "Sorry, you have to book another room"
-    
-Define list of the 6 tables
- - Function wich randomly attributes elements in a list
- Make a condition : the list should not have more than 4 people
-
-Function 
+Update the csv containing the names of the present people, and simply run main.py
--- main.py
+++ main.py
@@ -31,33 +31,34 @@ class OpenSpace:
         
         #random.shuffle(self.my_list)
         self.people = random.sample(self.my_list, 24)
-        print(self.people)
         self.remaining_persons = list[set(self.my_list) - set(self.people)]
 
         if len(self.my_list) >= 25:
-            print(f"Sorry {self.remaining_persons}, the first room is full. You should rent another one.")
+            print(f"\nSorry {self.remaining_persons}, the first room is full. You should rent another one.\n ")
 
         return self.my_list
         
+    """
     def create_table(self):
-        """
+        
         This method create table according to the way that any table should not have 
         more than 4 persons and should have more than one person
-        """
-        print(self.my_list)
-        self.tables = [[], [], [], [], [], []]
-        print(self.tables)
+        
         i = 0
 
         while self.my_list:
             if len(self.tables[i]) <= 4:  # Max 4 per table
                 self.tables[i].append(self.my_list.pop(0))
             i = (i + 1) % len(self.tables)  # Go to next table circularly
-
+"""
         
 
         # Print the tables
     def define_table(self):
+        """
+        Randomly assign tables to people.
+        """
+        self.tables = [[], [], [], [], [], []]
         i = 0
         while len(self.people)>= 4:
             self.tables[i].append(self.people[0:4])
@@ -69,25 +70,26 @@ class OpenSpace:
 
 
     def last_ones(self):
+        """
+        Dealing with the surplus of people
+        """
         if len(self.people) == 3:
-            tables[i].append(self.people[0:3]) 
+            self.tables[i].append(self.people[0:3]) 
 
         elif len(self.people) == 2:
-            tables[i].append(self.people[0:2])
+            self.tables[i].append(self.people[0:2])
         
         elif len(self.people) == 1:
-            tables[1].append(self.people[0]) 
+            self.tables[1].append(self.people[0]) 
 
-        return tables #print les listes créées
+        return self.tables #print les listes créées
         
    
-    
-            
-    test = OpenSpace()
-"""
-    test.uploaded_file()
-    test.random_list()
-    print(test.create_table())
-    print(test.define_table())
-    print(test.last_ones())
-"""
+         
+test = OpenSpace()
+test.uploaded_file()
+test.random_list()
+#test.create_table()
+print(test.define_table())
+test.last_ones()
+
