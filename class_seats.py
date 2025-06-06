b4ee34cd920953c48bfce5bdc0892e8fa3561912
kameni <kamaugust2005@yahoo.fr>
2025-06-06 14:53:59 +0200

    ton code modifiee

--- class_seats.py
+++ class_seats.py
@@ -2,7 +2,7 @@ import random
 
 people = ["Hajer", "Charly", "Choti", "Megan", "Moussa", "Yves", "Preeti", "Nadiya", "Aida", "Augustin", "Klebert", "Marc", "Sofia", "Younes", "Kenny", "Floriane", "Jordi", "Alberto", "Fang", "Caterina", "Yassine", "Dragos", "Hanieh", "Mengstu"]
 random.shuffle(people)
-print(people)
+#print(people)
 
 
 tables = [[],[],[],[],[],[]] # Number of tables defined for test
@@ -10,19 +10,16 @@ tables = [[],[],[],[],[],[]] # Number of tables defined for test
 def define_table(people):
     i = 0
     while len(people)> 0:
-        tables[i].append(people[0])
-        people.pop(0)
-        if len(tables[i] == 4):
-            continue
+        tables[i].append(people[0:4])
+        del people[0:4]
         i += 1
-                
-    else:
-        if len(people) == 3:
-            tables[i].append(people[0,2])
-        elif len(people) == 2:
-            tables[i].append(people[0,1])
-        elif len(people) == 1:
-            tables[i].append(people[0])
+
+    for idx, table in enumerate(tables, start=1):
+            print(f"Table {idx}: {table}")
+
+       
+
+       
 
     return tables #print les listes créées
         
