e94cf4a3830bb8c60ce4127a7fc19b45f866d4c7
kameni <kamaugust2005@yahoo.fr>
2025-06-06 16:00:45 +0200

    ok

--- class_seats.py
+++ class_seats.py
@@ -1,6 +1,6 @@
 import random
 
-people = ["Hajer", "Charly", "Choti", "Megan", "Moussa", "Yves", "Preeti", "Nadiya", "Aida", "Augustin", "Klebert", "Marc", "Sofia", "Younes", "Kenny", "Floriane", "Jordi", "Alberto", "Fang", "Caterina", "Yassine", "Dragos", "Hanieh", "Mengstu"]
+people = ["Hajer", "Charly", "Choti", "Megan", "Moussa", "Yves", "Preeti",  "Aida", "joseph"]
 random.shuffle(people)
 #print(people)
 
@@ -10,17 +10,29 @@ tables = [[],[],[],[],[],[]] # Number of tables defined for test
 def define_table(people):
     i = 0
     while len(people)> 0:
-        tables[i].append(people[0:4])
-        del people[0:4]
-        i += 1
+        if len(tables[i]) < 4:
+            tables[i].append(people.pop(0))
+            if len(tables[i]) == 0:
+                 del tables[i]
+            
+        else:
+            i += 1
 
-    for idx, table in enumerate(tables, start=1):
+    non_empty_tables = [table for table in tables if table]
+
+    if len(non_empty_tables[-1]) == 1:
+         non_empty_tables[-1].append(tables.pop([0][0]))
+
+
+    
+
+    for idx, table in enumerate(non_empty_tables, start=1):
             print(f"Table {idx}: {table}")
 
        
 
        
 
-    return tables #print les listes créées
+    return non_empty_tables #print les listes créées
         
 print(define_table(people))
